4c2937902dddf0429ec91d7483304be0d3305ff8
Pablo Terradillos <tehsis@gmail.com>
2015-04-29 03:15:28 -0300

    fix

--- index.jsx
+++ index.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import ModulesList from './src/ModulesList.jsx';
-import Search from './src/search.jsx';
-
-import modules from './modules.json';
-
-Search.search = (e) => console.log(e.target.value);
 
 let $searchInput = $('[name=modules-filter]');
-let modulesList = React.render(<ModulesList modules={modules} />, $('#canirequire-modules')[0]);
+let modulesList = React.render(<ModulesList />, $('#canirequire-modules')[0]);
 
-$searchInput.on('input', function(e) {
-  modulesList.setState({filter: $(this).val()});
-});
+$searchInput.on('input', () => modulesList.setState({filter: $(this).val()}));
