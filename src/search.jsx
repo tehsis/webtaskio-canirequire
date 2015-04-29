f8b7ef18260a4fe14fbdccd7ebf62bf7e4732ffb
Pablo Terradillos <tehsis@gmail.com>
2015-04-29 03:10:31 -0300

    removed unnecesary files

--- src/search.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default class Search extends React.Component {
-  render() {
-    return <input placeholder="e.g. mongodb" type="text" onChange={this.search} />
-  }
-}
