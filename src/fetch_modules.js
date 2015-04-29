d0d02d5bc20246930610da4129098addede54c4a
Pablo Terradillos <tehsis@gmail.com>
2015-04-29 13:58:01 -0300

    updated task

--- src/fetch_modules.js
+++ src/fetch_modules.js
@@ -3,7 +3,7 @@ export default function() {
     url: 'https://webtask.it.auth0.com/api/run/wt-tehsis-gmail_com-1',
     method: 'POST',
     headers: {
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsImtpZCI6IjIifQ.eyJqdGkiOiIzM2VlYjhkNDYxOTc0YmJmYmNmZWE0NzkxNDRhYTYyMyIsImlhdCI6MTQzMDI3NDQ0MiwiY2EiOlsiZDQ3ZDNiMzRkMmI3NGEwZDljYzgwOTg3OGQ3MWQ4Y2QiXSwiZGQiOjAsInVybCI6Imh0dHA6Ly90ZWhzaXMuZ2l0aHViLmlvL2NhbmlyZXF1aXJlL3Rhc2tzL2xpc3RfbW9kdWxlcy5qcyIsInRlbiI6Ii9ed3QtdGVoc2lzLWdtYWlsX2NvbS1bMC0xXSQvIn0.h8_6t1AdZhQK8v9jAFrn24RjPFxI--tUf2P75x3ju14'
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsImtpZCI6IjIifQ.eyJqdGkiOiIxMDdmNzMzMWFhOTQ0OGE4ODdjZTc4Y2JmN2NhNWE4ZCIsImlhdCI6MTQzMDMyNjU3MCwiY2EiOlsiZDQ3ZDNiMzRkMmI3NGEwZDljYzgwOTg3OGQ3MWQ4Y2QiXSwiZGQiOjAsInVybCI6Imh0dHA6Ly90aGVoc2lzLmdpdGh1Yi5pby93ZWJ0YXNraW8tY2FuaXJlcXVpcmUvdGFza3MvbGlzdF9tb2R1bGVzLmpzIiwidGVuIjoiL153dC10ZWhzaXMtZ21haWxfY29tLVswLTFdJC8ifQ.U6Q-bBqXaopxMwarPw2qPs1UinqGLoFDELKwfXEJMDA'  
     },
   });
 }
