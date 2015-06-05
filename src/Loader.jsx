12a0d5a8b29d4ccdbbb460cc351af32b94732a70
Pablo Terradillos <tehsis@gmail.com>
2015-06-05 04:57:35 -0300

    changed loader

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -4,16 +4,46 @@ export default class Loader extends React.Component {
   render() {
     return <div className="section center">
       <div className="preloader-wrapper big active">
-            <div className="spinner-layer spinner-blue-only">
-              <div className="circle-clipper left">
-                <div className="circle"></div>
-              </div><div className="gap-patch">
-                <div className="circle"></div>
-              </div><div className="circle-clipper right">
-                <div className="circle"></div>
-              </div>
-            </div>
-          </div>
+     <div className="spinner-layer spinner-blue">
+       <div className="circle-clipper left">
+         <div className="circle"></div>
+       </div><div className="gap-patch">
+         <div className="circle"></div>
+       </div><div className="circle-clipper right">
+         <div className="circle"></div>
+       </div>
+     </div>
+
+     <div className="spinner-layer spinner-red">
+       <div className="circle-clipper left">
+         <div className="circle"></div>
+       </div><div className="gap-patch">
+         <div className="circle"></div>
+       </div><div className="circle-clipper right">
+         <div className="circle"></div>
+       </div>
+     </div>
+
+     <div className="spinner-layer spinner-yellow">
+       <div className="circle-clipper left">
+         <div className="circle"></div>
+       </div><div className="gap-patch">
+         <div className="circle"></div>
+       </div><div className="circle-clipper right">
+         <div className="circle"></div>
+       </div>
+     </div>
+
+     <div className="spinner-layer spinner-green">
+       <div className="circle-clipper left">
+         <div className="circle"></div>
+       </div><div className="gap-patch">
+         <div className="circle"></div>
+       </div><div className="circle-clipper right">
+         <div className="circle"></div>
+       </div>
+     </div>
+   </div>
       </div>
   }
 };
