039309e6f4f7d6d951dc04acf4d8f1056bc55164
Pablo Terradillos <tehsis@gmail.com>
2015-06-05 06:38:56 -0300

    animate top bar

--- index.jsx
+++ index.jsx
@@ -7,3 +7,30 @@ let modulesList = React.render(<ModulesList />, $('#canirequire-modules')[0]);
 $searchInput.on('input', function(e) {
   modulesList.setState({filter: $(this).val()});
 });
+
+$(window).on('scroll', function() {
+    if (!isElementInViewport($('#canirequire-search'))) {
+      $('nav').removeClass('slidedup').addClass('animate-nav slideddown');
+      $('nav .header-text').addClass('animated-header-text');
+    } else {
+      $('nav').addClass('slidedup').removeClass('animate-nav slideddown');
+      $('nav .header-text').removeClass('animated-header-text');
+    }
+});
+
+
+
+function isElementInViewport (el) {
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+      el = el[0];
+  }
+
+  var rect = el.getBoundingClientRect();
+
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+  );
+}
