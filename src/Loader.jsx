0da48fb49f64c9d7963280f09a2ff3ef541601b5
Pablo Terradillos <tehsis@gmail.com>
2015-06-05 00:22:15 -0300

    Fixed styling

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default class Loader extends React.Component {
   render() {
-    return <div className="center">
+    return <div className="section center">
       <div className="preloader-wrapper big active">
             <div className="spinner-layer spinner-blue-only">
               <div className="circle-clipper left">
