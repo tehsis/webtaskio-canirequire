7f19721472db52ed7dcdc52aeae880a0870a96b8
Pablo Terradillos <tehsis@gmail.com>
2015-06-29 22:02:21 -0300

    Limit showed modules

--- src/ModulesList.jsx
+++ src/ModulesList.jsx
@@ -7,11 +7,13 @@ import fetch_modules from './fetch_modules';
 export default class ModulesList extends React.Component {
   constructor(props) {
     super(props);
+    this.loadMore = this.loadMore.bind(this);
     this.state = {
       filter: '',
       node_version: '',
       modules: [],
-      ready: false
+      ready: false,
+      limit: 12
     };
   }
 
@@ -26,21 +28,39 @@ export default class ModulesList extends React.Component {
       });
   }
 
+  loadMore() {
+    this.setState({
+      limit: this.state.limit + 12
+    });
+  }
+
   render() {
     if (!this.state.ready) return <Loader />;
 
-    let modules =
+    let filter_modules =
       this.state.modules
-        .filter(module => module.name.toLowerCase().includes(this.state.filter.toLowerCase()))
-        .map(module => <Module key={module.name + module.version} module={module} node_version={this.state.node_version} />);
+        .filter(module => module.name.toLowerCase().includes(this.state.filter.toLowerCase()));
+
+
+    let modules = filter_modules
+      .slice(0, this.state.limit)
+      .map(module => <Module key={module.name + module.version} module={module} node_version={this.state.node_version} />);
+
+    let loadMoreButton;
+    if (this.state.limit < filter_modules.length) {
+      loadMoreButton = <a className="center btn-floating btn-large waves-effect waves-light red" onClick={this.loadMore}><i className="material-icons">add</i></a>;
+    }
 
       return <div className="section white">
         <div className="row">
             <h5 className="col s12 m6 l6 webtask-red-text">Node Version: {this.state.node_version}</h5>
-            <h5 className="col s12 m6 l6 webtask-red-text">Number of modules: {modules.length}</h5>
+            <h5 className="col s12 m6 l6 webtask-red-text">Showing: {modules.length} of {filter_modules.length} modules </h5>
         </div>
         <div className="webtask-red">
           <div className="row">{modules}</div>
+        </div>
+        <div className="center">
+          {loadMoreButton}
         </div>
       </div>
   }
