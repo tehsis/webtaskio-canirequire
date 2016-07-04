c9c1e4f02c24e96331c330e35b625812920bc45f
Geoff Goodman <ggoodman@gmail.com>
2016-07-04 14:55:03 -0400

    Update module listing code
    We made a change in the directory layout. This PR adjusts your webtask to understand this new layout.

--- tasks/list_modules.js
+++ tasks/list_modules.js
@@ -1,31 +1,60 @@
-var fs = require('fs');
-var natives = Object.keys(process.binding("natives")).filter(function(nativeDep) {
-  return nativeDep[0] !== '_';
-}).map(function(native) {
-  return {version: 'native', name: native};
-});
+const Fs = require('fs');
+const Verquire = require('verquire');
+const _ = require('lodash');
 
-return function(cb) {
-  cb(null, {
-    node_version: process.version,
-    modules: natives.concat(fs.readdirSync(__dirname + '/node_modules')
-      .filter(function(dir) {
-        return dir[0] !== '.';
-      })
-      .reduce(function(prev, dep) {
-        var versions = require('verquire').modules;
-        var depObj = JSON.parse(fs.readFileSync(__dirname + '/node_modules/' + dep + '/package.json'));
 
-        if (!Array.isArray(versions[depObj.name])) return prev;
+const natives = Object.keys(process.binding("natives"))
+    .filter(nativeDep => nativeDep[0] !== '_')
+    .reduce((natives, name) => {
+        natives[name] = { name, versions: ['native'] };
+        
+        return natives;
+    }, {});
+const versions = module.paths.reduce((versions, basePath) => {
+    try {
+        Fs.readdirSync(basePath)
+            .filter(name => name[0] !== '.')
+            .forEach(name => {
+                const packageJson = require(`${basePath}/${name}/package.json`);
+                
+                if (!versions[name]) {
+                    versions[name] = {
+                        name,
+                        versions: [],
+                        homepage: packageJson.homepage,
+                    };
+                }
+                
+                versions[name].versions.push(packageJson.version);
+                
+                _.forEach(Verquire.modules[name], version => {
+                    if (versions[name].versions.indexOf(version) === -1) {
+                        versions[name].versions.push(version);
+                    }
+                });
+            });
+    } catch (e) {
+        if (e.code !== 'ENOENT') {
+            console.warn('WARN', e.message);
+        }
+    }
+    
+    return versions;
+}, natives);
+const modules = _.reduce(versions, (modules, entry) => {
+    entry.versions.forEach(version => modules.push({
+        name: entry.name,
+        homepage: entry.homepage,
+        version,
+    }));
+    
+    return modules;
+}, []);
 
-        return prev.concat(versions[depObj.name].map(function(version) {
-          return {
-            name : depObj.name,
-               version: version,
-               homepage: depObj.homepage
-          };
-        }));
-      }, []))
-  });
+
+module.exports = cb => {
+    cb(null, {
+        node_version: process.version,
+        modules,
+    });
 };
-
