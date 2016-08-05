2f4265ac61bb013bcaa0fa9364f62f4e814e9c31
Pablo Terradillos <tehsis@gmail.com>
2016-08-05 13:51:41 -0300

    fix modules

--- tasks/list_modules.js
+++ tasks/list_modules.js
@@ -1,60 +1,28 @@
+'use npm';
 const Fs = require('fs');
 const Verquire = require('verquire');
-const _ = require('lodash');
+const _ = require('lodash@4.8.2');
 
 
 const natives = Object.keys(process.binding("natives"))
     .filter(nativeDep => nativeDep[0] !== '_')
-    .reduce((natives, name) => {
-        natives[name] = { name, versions: ['native'] };
-        
-        return natives;
-    }, {});
-const versions = module.paths.reduce((versions, basePath) => {
-    try {
-        Fs.readdirSync(basePath)
-            .filter(name => name[0] !== '.')
-            .forEach(name => {
-                const packageJson = require(`${basePath}/${name}/package.json`);
-                
-                if (!versions[name]) {
-                    versions[name] = {
-                        name,
-                        versions: [],
-                        homepage: packageJson.homepage,
-                    };
-                }
-                
-                versions[name].versions.push(packageJson.version);
-                
-                _.forEach(Verquire.modules[name], version => {
-                    if (versions[name].versions.indexOf(version) === -1) {
-                        versions[name].versions.push(version);
-                    }
-                });
-            });
-    } catch (e) {
-        if (e.code !== 'ENOENT') {
-            console.warn('WARN', e.message);
-        }
-    }
-    
-    return versions;
-}, natives);
-const modules = _.reduce(versions, (modules, entry) => {
-    entry.versions.forEach(version => modules.push({
-        name: entry.name,
-        homepage: entry.homepage,
-        version,
-    }));
-    
-    return modules;
-}, []);
+    .map(dep => ({name: dep, version: 'native'}));
 
+
+const modules = _.flatMap(Verquire.modules, (versions, module_name) => {
+   return versions.map((version) => {
+       const moduleObj = {
+         name: module_name,
+         version: version
+       };
+
+       return moduleObj;
+   });
+}).concat(natives);
 
 module.exports = cb => {
     cb(null, {
         node_version: process.version,
-        modules,
+        modules
     });
 };
