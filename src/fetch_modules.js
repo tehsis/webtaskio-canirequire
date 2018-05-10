623be25953a5dfc1615a3e757ad681488d778d06
Pablo Terradillos <tehsis@gmail.com>
2018-05-10 15:53:07 -0300

    Update fetch_modules.js

--- src/fetch_modules.js
+++ src/fetch_modules.js
@@ -3,7 +3,7 @@ var algo = ``;
 export default function(token, url) {
   if (!token || !url) {
     return $.ajax({
-      url: 'https://webtask.it.auth0.com/api/run/wt-tehsis-gmail_com-1',
+      url: 'https://wt-tehsis-gmail_com-0.sandbox.auth0-extend.com/list_modules',
       method: 'POST',
       headers: {
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsImtpZCI6IjIifQ.eyJqdGkiOiJmZGZiOWU2MjQ0YjQ0YWYyYjc2YzAwNGU1NjgwOGIxNCIsImlhdCI6MTQzMDMyNjc4MiwiY2EiOlsiZDQ3ZDNiMzRkMmI3NGEwZDljYzgwOTg3OGQ3MWQ4Y2QiXSwiZGQiOjAsInVybCI6Imh0dHA6Ly90ZWhzaXMuZ2l0aHViLmlvL3dlYnRhc2tpby1jYW5pcmVxdWlyZS90YXNrcy9saXN0X21vZHVsZXMuanMiLCJ0ZW4iOiIvXnd0LXRlaHNpcy1nbWFpbF9jb20tWzAtMV0kLyJ9.MJqAB9mgs57tQTWtRuZRj6NCbzXxZcXCASYGISk3Q6c'
