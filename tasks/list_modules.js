166a71dc34476fc7d7bb9b36f575a7a06abce86c
Geoff Goodman <geoff@auth0.com>
2018-05-29 10:22:29 -0400

    Update list_modules webtask to be dependency-free

--- tasks/list_modules.js
+++ tasks/list_modules.js
@@ -1,6 +1,4 @@
-const Fs = require('fs');
 const Path = require('path');
-const _ = require('lodash');
 
 const abcsort = function (a, b) {
   if (a.name < b.name) {
@@ -23,16 +21,18 @@ const natives = Object.keys(process.binding("natives"))
 const manifest = require(Path.join(process.env.VERQUIRE_DIR, 'packages.json'));
 
 
-const modules = _.reduce(manifest, (acc, versions, module_name) => {
-     versions.forEach((version) => {
+const modules = Object.keys(manifest).reduce((acc, module_name) => {
+    const versions = manifest[module_name];
+
+    versions.forEach((version) => {
          const moduleObj = {
            name: module_name,
            version: version
          };
-  
+
          acc.push(moduleObj);
      });
-     
+
      return acc;
   }, [])
   .sort(abcsort);
